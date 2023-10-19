5bec55a8e7da5efc4f76d2c004ccf0b542ca5522
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2023-10-20 05:27:36 +0900

    a little bit modified

--- day4/TryOverload.java
+++ day4/TryOverload.java
@@ -48,7 +48,7 @@ public class TryOverload {
     }
 
     public void times(String str1, String str2) {
-        System.out.println("\"" + str1 + "\"" + " + " + "\"" + str2 + "\"" + " = " + str1 + str2);
+        System.out.println("\"" + str1 + "\"" + " + " + "\"" + str2 + "\"" + " = " + "\"" + str1 + str2 + "\"");
     }
 
     public static void main(String[] argv) {
