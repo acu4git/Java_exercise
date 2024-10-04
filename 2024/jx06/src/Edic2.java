ea262b61d7a228d4c4f7aca40269683d686300a8
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2024-10-05 00:11:05 +0900

    gya

--- 2024/jx06/src/Edic2.java
+++ 2024/jx06/src/Edic2.java
@@ -2,22 +2,24 @@ import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.regex.PatternSyntaxException;
 import java.awt.*;
 import javax.swing.*;
 
-public class Edic extends JFrame implements ActionListener {
+public class Edic2 extends JFrame implements ActionListener {
   private Container c;
   private JTextField textField;
   private JButton btnTrans, btnClear, btnQuit;
   private JLabel transLabel;
   private JPanel p;
+  private HashMap<String, String> dictionary;
 
   private void setLabel(String str) {
     transLabel.setText(str);
   }
 
-  Edic() {
+  Edic2() {
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setSize(400, 200);
     setTitle("Edic");
@@ -25,6 +27,7 @@ public class Edic extends JFrame implements ActionListener {
     c = getContentPane();
     c.setLayout(new GridLayout(3, 1));
     p = new JPanel(new FlowLayout());
+    dictionary = new HashMap<>();
 
     textField = new JTextField("", 12);
     btnTrans = new JButton("Translate");
@@ -49,6 +52,11 @@ public class Edic extends JFrame implements ActionListener {
     String[] tokens = new String[2];
 
     System.out.println("target word: " + word);
+    if (dictionary.containsKey(word)) {
+      System.out.println("Success to use hashMap!");
+      setLabel(dictionary.get(word));
+      return;
+    }
 
     try (BufferedReader buffReader = new BufferedReader(new FileReader("dic.txt"));) {
       while (true) {
@@ -63,6 +71,7 @@ public class Edic extends JFrame implements ActionListener {
         // debug
         System.out.println("tokens[0]: " + tokens[0]);
         if (tokens[0].equals(word)) {
+          dictionary.put(word, tokens[1]);
           setLabel(tokens[1]);
           System.out.println("result: " + tokens[1]);
           break;
@@ -88,7 +97,7 @@ public class Edic extends JFrame implements ActionListener {
   }
 
   public static void main(String[] args) {
-    Edic edic = new Edic();
+    Edic2 edic = new Edic2();
     edic.setVisible(true);
   }
 }
